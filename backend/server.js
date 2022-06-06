dbe1b85e872756c20d5be566096bcb244afe0369
Alessandro Parrilla <paggio96@gmail.com>
2022-06-06 20:33:09 +0200

    ROUTING WITH GET METHOD AND MAKE A GET REQUEST ON POSTMAN

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 8000;
 
 const app = express()
 
+//ROUTING fa riferimento alla definizione di endpoint dell’applicazione (URI) e alla loro modalità di risposta alle richieste del client.
+
+//GET method route
+app.get('/', (req, res) => {
+  // res.send('Hello')  //su postman effettua una richiesta get ad http://localhost:5000
+  res.status(200).json({message: 'Welcome to the Support Desk API'})
+})
 
 
 app.listen(PORT, () => console.log(`Example app listening on ${PORT}`))
