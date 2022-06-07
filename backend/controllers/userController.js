a888f080ba3b5f28b5f0514d98cf825c691115e6
Alessandro Parrilla <paggio96@gmail.com>
2022-06-07 21:05:34 +0200

    Error & Exception Handling, express-async-handler installed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,29 @@
-const registerUser = (req, res) => {
-  res.send('Register User')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// @desc  Register a new user
+// @route  /api/users
+// @access Public
+const registerUser = asyncHandler(async(req, res) => {
+  console.log(req.body); 
+  //get undefined because we need to set the body parser middleware in server.js
+
+  const {name, email, password} = req.body
+
+  // Validadion
+  if(!name || !email || !password){
+    res.status(400)
+    throw new Error('Please include all fields')
+  }
+
+  res.send('Register User')
+})
+
+// @desc  Login a user
+// @route  /api/users/login
+// @access Public
+const loginUser = asyncHandler(async(req, res) => {
   res.send('Login User')
-}
+})
   
   module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 8000;
 
 const app = express()
+
+app.use(express.json()) //for body -> raw (json)
+app.use(express.urlencoded({extended: false})) // for body -> x-www-urlformencoded
+//NOW WE CAN GET DATA FROM THE BODY ({name, email, password})
 
 //ROUTING fa riferimento alla definizione di endpoint dell’applicazione (URI) e alla loro modalità di risposta alle richieste del client.
 
@@ -13,9 +18,11 @@ app.get('/', (req, res) => {
   res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
 
-// Connect endpoint to userRoutes
+// Routes
 // in postman you'll get a "Register Route" response or "Login Route" if endpoint is /api/users/login
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 
 app.listen(PORT, () => console.log(`Example app listening on ${PORT}`))
