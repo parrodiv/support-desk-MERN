b37b609f34bd06552b10fd704f623ca1c811b42b
Alessandro Parrilla <paggio96@gmail.com>
2022-06-22 22:45:39 +0200

    Add Tickets to Redux

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -4,13 +4,13 @@ const API_URL = '/api/users' //register user
 
 //Register User
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData);
+  const response = await axios.post(API_URL, userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
+    localStorage.setItem('user', JSON.stringify(response.data))
   }
 
-  return response.data;
+  return response.data
   // {   _id: user._id,
   //     name: user.name,
   //     email: user.email,
@@ -21,26 +21,26 @@ const register = async (userData) => {
 //Logout User
 const logout = () => localStorage.removeItem('user')
 
-// Login User 
+// Login User
 const login = async (userData) => {
   const response = await axios.post(`${API_URL}/login`, userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
+    localStorage.setItem('user', JSON.stringify(response.data))
   }
-  
-  return response.data 
+
+  return response.data
   // {   _id: user._id,
   //     name: user.name,
   //     email: user.email,
   //     token: generateToken(user._id)
   // }
-} 
+}
 
 const authService = {
   register,
   logout,
-  login
+  login,
 }
 
 export default authService
