8ec53d2120e71cde0eedb2dd640bf74aa79868ca
Alessandro Parrilla <paggio96@gmail.com>
2022-06-24 12:26:28 +0200

    Listing Tickets in UI

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getTickets, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 function Tickets() {
   const { tickets, isSuccess, isLoading, isError, message } = useSelector(
@@ -15,17 +16,18 @@ function Tickets() {
   //you just won't reset success state when you navigate away from the /tickets page, so next time you go to /tickets success will be true, so if you did have a error in fetching the tickets you may not be able to handle that and display something appropriate to the user.
   // all the first one is doing is returning a function to reset some state which will run when the user navigates away from /tickets i.e. when the Tickets component unmounts.
   useEffect(() => {
+     if (!isSuccess) {
+       dispatch(getTickets())
+     }
+
     return () => {
       if(isSuccess){
         dispatch(reset())
       }
     }
-  },[isSuccess])
+  },[dispatch, isSuccess])
 
 
-  useEffect(() => {
-    dispatch(getTickets())
-  }, [dispatch])
 
   if (isLoading) {
     return <Spinner />
@@ -34,9 +36,21 @@ function Tickets() {
   // console.log(tickets) // [{..}, {..}]
 
   return (
-    <div>
+    <>
+      <BackButton url='/' />
       <h1>Tickets</h1>
-    </div>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   )
 }
 
