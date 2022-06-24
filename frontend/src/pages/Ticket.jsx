8889475ca6cf3368f7d0702968c18c062122885f
Alessandro Parrilla <paggio96@gmail.com>
2022-06-24 20:30:29 +0200

    Single Ticket Display

---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,59 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTicket, reset } from '../features/tickets/ticketSlice'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import { toast } from 'react-toastify'
+
+const Ticket = () => {
+  const { ticket, isError, isSuccess, isLoading, message } = useSelector(
+    (state) => state.tickets
+  )
+
+  const dispatch = useDispatch()
+  const params = useParams()
+
+  const { ticketId } = useParams() //get ticketId from URL
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    dispatch(getTicket(ticketId))
+  }, [isError, message, ticketId])
+  //I don't put dispatch as dependency here because an infinite cycle would arise
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h3>Something went wrong</h3>
+  }
+
+  return (
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton url='/tickets' />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('it-IT')}
+        </h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Ticket
