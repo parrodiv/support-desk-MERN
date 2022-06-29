1b4f603eb464006181991c4d254bb3247acf233a
Alessandro Parrilla <paggio96@gmail.com>
2022-06-29 08:34:56 +0200

    Display Notes

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,14 +1,22 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getTicket, closeTicket } from '../features/tickets/ticketSlice'
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice'
+//nel caso in cui sia già stato utilizzato reset è possibile cambiare il nome per poter usare due tipi di reset da due Slice differenti
+
 import { useParams, useNavigate } from 'react-router-dom'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
+import NoteItem from '../components/NoteItem'
 import { toast } from 'react-toastify'
 
 const Ticket = () => {
-  const {ticket, isError, isSuccess, isLoading, message } = useSelector(
+  const { ticket, isError, isSuccess, isLoading, message } = useSelector(
     (state) => state.tickets
+  )
+
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
   )
 
   const dispatch = useDispatch()
@@ -23,18 +31,19 @@ const Ticket = () => {
     }
 
     dispatch(getTicket(ticketId))
+    dispatch(getNotes(ticketId))
   }, [isError, message, ticketId])
   //I don't put dispatch as dependency here because an infinite cycle would arise
 
   // Close ticket
   const onTicketClose = () => {
     dispatch(closeTicket(ticketId))
-     //change the status of ticket on backend
+    //change the status of ticket on backend
     toast.success('Ticket Closed')
     navigate('/tickets')
   }
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />
   }
 
@@ -56,15 +65,22 @@ const Ticket = () => {
           Date Submitted: {new Date(ticket.createdAt).toLocaleString('it-IT')}
         </h3>
         <hr />
-        <div className="ticket-desc">
+        <div className='ticket-desc'>
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {notes.map(note => (
+        <NoteItem key={note._id} note={note} />
+      ))}
 
       {/* Show button only if status of ticket isn't equal to closed */}
       {ticket.status !== 'closed' && (
-        <button className="btn btn-block btn-danger" onClick={onTicketClose}>Close Ticket</button>
+        <button className='btn btn-block btn-danger' onClick={onTicketClose}>
+          Close Ticket
+        </button>
       )}
     </div>
   )
